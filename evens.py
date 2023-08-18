82481d637871f1d5c847b5bd475ccea33625c70e
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-08-18 19:48:25 +0000

    Add code in evens.py file

--- evens.py
+++ evens.py
@@ -8,10 +8,7 @@ def even_number_of_evens(numbers):
     """
 
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            evens = 0
+        evens = 0
 
         for n in numbers:
             if n % 2 == 0:
@@ -26,4 +23,4 @@ def even_number_of_evens(numbers):
 
 
 if __name__ == '__main__':
-    print(even_number_of_evens(5))   
+    even_number_of_evens([2, 1, 4])   
